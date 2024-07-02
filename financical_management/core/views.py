fdc39ac8bf517c6b9aa4f54b02d1dd3745c4aef8
atharvarakshak <atharvarakshak23@gmail.com>
2024-07-02 23:03:43 +0530

    ver 1

--- financical_management/core/views.py
+++ financical_management/core/views.py
@@ -3,9 +3,23 @@ from django.http import HttpResponse
 
 
 from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 
 
+bot = ChatBot('chatbot',read_only=False,logic_adapters=['chatterbot.logic.BestMatch'])
 
+list_to_train =[
+    "hi",
+    "hi there",
+    "Whats your name?",
+    "I'm just a chatbot",
+    "Give me some finance tips",
+    "What is your monthly income?",
+    "I earn 1000 dollars per month",
+
+]
+
+ListTrainer(bot).train(list_to_train)
 # Create your views here.
 
 def index(request):
@@ -16,4 +30,7 @@ def index(request):
 def getResponse(request):
     userMessage = request.GET.get('userMessage')
 
-    return HttpResponse(userMessage)
+    chatResponse = str(bot.get_response(userMessage))
+
+
+    return HttpResponse(chatResponse)
